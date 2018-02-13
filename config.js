77b74d16ea7abe1fd7281f65263ad858a40469b4
FarajiA <sacbeme04@yahoo.com>
2018-02-12 18:28:53 -0600

    update favicon

--- config.js
+++ config.js
@@ -3,14 +3,11 @@
     config.urls = {
         github: 'https://api.github.com/'
     };
-
-    config.OAuth = "086bf22fba2886d8b337553fb9c9ba41eb02a159";
+    
     config.headers = {
             'Accept': '*/*',
             'User-Agent': 'NodeApp',
-            'Content-Type': 'application/json',
-            'Authorization': 'token ' + config.OAuth
-
+            'Content-Type': 'application/json'
     };
  
 })(module.exports);
